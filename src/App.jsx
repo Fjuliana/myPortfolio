1acc27a725170a0e59253f3955e055f34ae9af5e
Faith Lema <faithjuliana44@gmail.com>
2026-01-17 22:44:38 +0800

    contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './index.css'
+import styles from "./App.module.css";
+import { About } from "./components/about/About.jsx";
+import { Experience } from "./components/Experience/Experience.jsx";
+import { Contact } from "./components/contact/Contact.jsx";
+import { Hero } from "./components/Hero/Hero.jsx";
+import { Navbar } from "./components/navbar/Navbar.jsx";
+import { Projects } from "./components/projects/Projects.jsx";
 
 function App() {
-  
-
-
   return (
-    <>
-      <div>Hello world</div>
-    </>
-  )
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
-export default App
+export default App;
